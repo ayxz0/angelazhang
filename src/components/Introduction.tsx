ac20f196485c8e8c58004d1da101a119039149a6
CTRLFUSION <angelaz.yuxi@gmail.com>
2023-08-17 15:45:29 -0700

    added name to intro component

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,12 @@
+export function Introduction() {
+    return (
+        <>
+            <div className="z-10 max-w-5xl w-full items-center justify-between lg:flex">
+                <span className="text-7xl font-serif font-bold">Angela Zhang</span>
+                <div className="w-5/12">
+                    <a href="./" className="text-3xl font-mono font-thin text-slate-500 hover:text-slate-600">@ayxz0</a>
+                </div>
+            </div>
+        </>
+    )
+}
