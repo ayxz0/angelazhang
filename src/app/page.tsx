2645d29675b6e2fcb6939d3ce725a172bf29b2d4
CTRLFUSION <angelaz.yuxi@gmail.com>
2023-08-18 21:22:14 -0700

    added pfp space and description to intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Introduction } from "@/components/Introduction";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between py-16 px-24">
+        <div className="z-10 max-w-5xl w-full justify-between font-mono text-sm lg:flex">
             <Introduction />
         </div>
     </main>
