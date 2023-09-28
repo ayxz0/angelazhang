28a340782416c1e2ffa9f917449ba4d7b50fafcc
CTRLFUSION <angelaz.yuxi@gmail.com>
2023-09-27 23:04:48 -0700

    minor fixes

--- src/app/CustomCursor.tsx
+++ src/app/CustomCursor.tsx
@@ -5,7 +5,7 @@ const CustomCursor = () => {
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const onMouseMove = (e:any) => {
+    const onMouseMove = (e:MouseEvent) => {
       setCursorPosition({ x: e.clientX, y: e.clientY });
     };
 
@@ -18,7 +18,7 @@ const CustomCursor = () => {
 
   return (
     <div
-      className="fixed h-screen inset-0 transition duration-300 lg:absolute"
+      className="h-screen inset-0 transition duration-300 lg:absolute"
       style={{background: `radial-gradient(600px at ${cursorPosition.x}px ${cursorPosition.y}px, rgba(12, 116, 173, 0.15), transparent 80%)`}}
     ></div>
   );
