d9673f5b6a69f1348705771a14b1a3699e733a1c
CTRLFUSION <angelaz.yuxi@gmail.com>
2023-11-02 13:14:29 -0700

    page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       const handleScroll = () => {
         const skillsSection = document.querySelector('#skills')!;       
         const projsSection = document.querySelector('#projects')!;  
-        const activeSec = projsSection?.getBoundingClientRect().top <=0 ? "projects" 
+        const activeSec = projsSection?.getBoundingClientRect().top <=5 ? "projects" 
             : (skillsSection?.getBoundingClientRect().top <=0 ? "skills" : "main"); 
         setActiveSection(activeSec);
       };
@@ -26,12 +26,12 @@ export default function Home() {
     }, []);
     
     return (
-        <main className="flex min-h-screen flex-col items-center">
-            {activeSection !== "main" && <div className={`w-full z-10 top-0 justify-center lg:flex border-cyan-900 border-b-[1px] sticky`}>
+        <main className="flex flex-col items-center">
+            {activeSection !== "main" && 
+            <div className={`w-full z-10 top-0 justify-center lg:flex border-cyan-900 border-b-[1px] sticky`}>
                 <Header activeSection={activeSection}/>
             </div>}
             <section id="main" className="bg-black">
-                <CustomCursor />
                 <LandingPage />           
             </section>
             <section id="skills" className="bg-gradient-to-r from-zinc-900 from-60% to-zinc-800 to-100% pt-12 w-full h-screen text-white">
@@ -53,12 +53,12 @@ export default function Home() {
                     <Projects />
                 </div>
             </section>
-            <div id="footer" className="flex text-zinc-600 justify-center pt-[7vh] font-thin text-sm pb-4"> 
+            <div id="footer" className="flex text-zinc-600 justify-center pt-[7vh] font-thin text-sm"> 
                 <div>
                     <div className="flex justify-center">
-                        Angela Zhang &copy; 2023; 
-                        Built with Next.js and Tailwind CSS, deployed with Vercel.
-                    </div>
+                    Angela Zhang &copy; 2023; 
+                    Built with Next.js and Tailwind CSS, deployed with Vercel. 
+                </div>
                     <div className="flex justify-center">
                         Animation inspired by <a className="hover:text-zinc-300" href="https://chronark.com/"> &nbsp;chronark</a>.
                     </div> 
